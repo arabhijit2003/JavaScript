085ef7fdbbcb7760c675734e005b93a595bd8638
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-02 15:51:02 +0000

    datatype added

---
+++ Basics/datatype.js
@@ -0,0 +1,13 @@
+"use strict" //for new format of coding
+let state;
+// number => 2 to power 53
+// bigint
+// string => II II
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+I
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
