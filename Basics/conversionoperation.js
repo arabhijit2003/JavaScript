aa1ce104e620aaca035adb22ed16fb01eadd07fe
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-02 19:35:59 +0000

    conversion and operations

--- Basics/conversionoperation.js
+++ Basics/conversionoperation.js
@@ -1,10 +1,10 @@
-let score = '33'
-let scoreABC = '33abc'
-console.log(typeof score);
-let valuNumber = Number(score)
-console.log(typeof valuNumber);
-let valuNumberABC = Number(scoreABC)
-console.log(valuNumberABC) //gives NaN(Not a Number)
+// let score = '33'
+// let scoreABC = '33abc'
+// console.log(typeof score);
+// let valuNumber = Number(score)
+// console.log(typeof valuNumber);
+// let valuNumberABC = Number(scoreABC)
+// console.log(valuNumberABC) //gives NaN(Not a Number)
 
 // convertion to string
 // "33"=> convert to number
@@ -12,4 +12,11 @@ console.log(valuNumberABC) //gives NaN(Not a Number)
 // bools => 1/2 
 
 //******************Operations***********************
+
+// let value = 3
+// console.log(-value);
+console.log(null>0); //false //convert null to 0
+console.log(null==0); //false //dont convert null to zero...its euqlity operator
+console.log(null===0); //strict check.....check datatype too
+
 
--- Basics/datatype.js
+++ Basics/datatype.js
@@ -1,13 +1,30 @@
-"use strict" //for new format of coding
-let state;
-// number => 2 to power 53
-// bigint
-// string => II II
-// boolean => true/false
-// null => standalone value
-// undefined =>
-// symbol => unique
-I
-// object
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// "use strict" //for new format of coding
+// let state;
+// // number => 2 to power 53
+// // bigint
+// // string => II II
+// // boolean => true/false
+// // null => standalone value
+// // undefined =>
+// // symbol => unique
+// I
+// // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// //types of datatype
+// //primitive -7 
+// //string, number, bool, null, undefined, Symbol(make unique),bigint
+// //non primitve
+// //array, objects, functions
+
+// JS is dynamic ...hold diff values during run time 
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123') //hold same value bt datatype diff 
+
+// const arr = ["akash","abhijit"]
+
+// const myFunction = function () {
+//     console.log("Hello world");
+//     }   //function 
