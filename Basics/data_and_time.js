7cb125e2a0fd6eb7d032279b49253dc5021babbf
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-03 14:07:20 +0000

    date_time

---
+++ Basics/data_and_time.js
@@ -0,0 +1,8 @@
+// //Dates
+//  let myDate = new Date();
+// //  console.log(myDate.toTimeString());
+// //  console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// console.log((Date(2024, 08, 03)).toString)
+let timeStamp = Date.now()/(1000*60)
+console.log(Math.floor(timeStamp));
