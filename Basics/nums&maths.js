54e2ad0caf4189f1675ab74f48aefbc30e2e9a05
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-03 11:18:38 +0000

    maths

---
+++ Basics/nums&maths.js
@@ -0,0 +1,24 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number (100) //datatype number is assigned and after this
+//                                 // can be used for converting number to other datatypes
+// console.log(balance.toString); //convert to string
+// console.log(balance.toFixed(2)); //fix no of digits after decimal 
+//                     //toPrecision ...marks the no of precise digits
+// let hunds = 1000000000
+// console.log(hunds.toLocaleString('en-IN')); //number system
+
+//--------------------------Maths-------------------------------
+
+// console.log(Math.abs(-4));
+//                 //round
+//                 // ceil ...choose the top round
+//                 // floor ......take the lowest value
+// console.log(Math.sqrt(4.3));
+//console.log((Math.random()*10)+1); //gives min 1
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)+min));
+ 
+            
