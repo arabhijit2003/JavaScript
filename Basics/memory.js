4b42820c51eb68a909d982b027a596aeb3cf4d30
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-03 05:26:07 +0000

    memories

---
+++ Basics/memory.js
@@ -0,0 +1,3 @@
+ //stack(primitive), heap(non-primitive)
+ //in stack we copy data 
+ //in heap only referance is given 
---
+++ Basics/strings.js
@@ -0,0 +1,6 @@
+const name ='abhijit'
+const count = 50 
+// console.log(name+" " + count + " value"); //outdated 
+
+// console.log(`Hello my name is ${name} and count is ${count}`); //new way 
+console.log(name.toUpperCase());
