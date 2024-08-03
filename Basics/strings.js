756b1fa8f1be2b2e50252493e271376459d57484
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-03 07:25:51 +0000

    strings

--- Basics/strings.js
+++ Basics/strings.js
@@ -1,6 +1,18 @@
-const name ='abhijit'
+const name ='   abhijit    '
 const count = 50 
+url = 'abhijit-roy-2003'
 // console.log(name+" " + count + " value"); //outdated 
 
 // console.log(`Hello my name is ${name} and count is ${count}`); //new way 
-console.log(name.toUpperCase());
+//console.log(name.toUpperCase());
+console.log(name)//length());
+                //toUpperCase()
+                //chatAt(index)
+                //indexOf('char')
+//substring //slice
+console.log(name.trim()); //trim //trimStart //trimEnd
+
+//replace 
+
+console.log(url.split("-")); //convert to array divided by the delimiter
+
