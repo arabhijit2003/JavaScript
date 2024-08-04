22ae43ad397256a6faf9b4b0104aaa72262088ad
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-04 05:21:58 +0000

    array

---
+++ Basics/objects.js
@@ -0,0 +1,25 @@
+//singleton
+//object literals
+const sym = Symbol("key1")
+
+const jsUser =  {
+    name : "Abhijit",
+    age : 21 ,
+    loc : "Agt",
+    email : "arabhijit2003@gmail.com",
+    isLoggedin : true
+ //   [sym] : "mykey1"
+
+}  //format of object 
+console.log(jsUser);
+
+Object.create //constructor method 
+
+console.log(jsUser['name']);
+
+jsUser.greeting = function(){
+    console.log(`Hello user ${this.name}`);
+    
+}
+
+console.log(jsUser.greeting());
