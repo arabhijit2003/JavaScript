1c75c7e63d4553ae14d4f710e0546443424d833c
Abhijit Roy <85212325+arabhijit2003@users.noreply.github.com>
2024-08-06 19:24:14 +0000

    reduce

--- Basics/filter_map_reduce.js
+++ Basics/filter_map_reduce.js
@@ -47,4 +47,40 @@
 
 // console.log(userBooks);
 
-const myNumbers = [1,2,3,4,5,6,7,8,9,100]
+//const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+//newNums = myNumbers.map((num) => num+10)
+//console.log(newNums);
+
+// const newNums = myNumbers.map( (num) => num*10)
+//                         .map((num) => num*2 )
+//                         .filter((nums) => nums >=40)
+
+//console.log(newNums); 
+
+// //reduce 
+// const arr = [1,2,3,4,5]
+// const initial = 0
+// const sumWithInitial = arr.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue, initial
+// )
+// console.log(sumWithInitial); //0+1+2+3+4+5 = 15
+
+const shoppingCart = [
+    {
+    itemName: "js course",
+    price: 2999
+    },
+    {
+    itemName: "python course",
+    price: 999
+    },
+    {
+    itemName: "ds course",
+    price: 12999
+    }
+    ]
+
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
